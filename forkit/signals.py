bfe2985bc7347acf77e3d68b2785ef6672a434a6
Francisco Pantoja <fcopantoja@gmail.com>
2023-07-25 07:54:11 -0600

    Update signals.py

--- forkit/signals.py
+++ forkit/signals.py
@@ -1,13 +1,25 @@
 from django.dispatch import Signal
 
-pre_reset = Signal(providing_args=('reference', 'instance', 'config'))
-post_reset = Signal(providing_args=('reference', 'instance'))
+pre_reset = Signal()
+post_reset = Signal()
 
-pre_fork = Signal(providing_args=('reference', 'instance', 'config'))
-post_fork = Signal(providing_args=('reference', 'instance'))
+pre_fork = Signal()
+post_fork = Signal()
 
-pre_diff = Signal(providing_args=('reference', 'instance', 'config'))
-post_diff = Signal(providing_args=('reference', 'instance', 'diff'))
+pre_diff = Signal()
+post_diff = Signal()
 
-pre_commit = Signal(providing_args=('reference', 'instance'))
-post_commit = Signal(providing_args=('reference', 'instance'))
+pre_commit = Signal()
+post_commit = Signal()
+
+# pre_reset = Signal(providing_args=('reference', 'instance', 'config'))
+# post_reset = Signal(providing_args=('reference', 'instance'))
+
+# pre_fork = Signal(providing_args=('reference', 'instance', 'config'))
+# post_fork = Signal(providing_args=('reference', 'instance'))
+
+# pre_diff = Signal(providing_args=('reference', 'instance', 'config'))
+# post_diff = Signal(providing_args=('reference', 'instance', 'diff'))
+
+# pre_commit = Signal(providing_args=('reference', 'instance'))
+# post_commit = Signal(providing_args=('reference', 'instance'))
